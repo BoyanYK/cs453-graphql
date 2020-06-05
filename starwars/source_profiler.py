a8f95283eef5d58bb18de0eed3ac4e3200cb2b80
devturnip <ossig.anthony@gmail.com>
2020-06-05 15:42:17 +0900

    test function wrapping

--- starwars/source_profiler.py
+++ starwars/source_profiler.py
@@ -1,7 +1,9 @@
 import ast
 import astor
 import logging
+import copy
 
+import graphene
 from graphene.test import Client
 
 
@@ -21,12 +23,56 @@ class profiler_transformer(ast.NodeTransformer):
             keywords=[]
         ))
         node.body.insert(0, new_node)
+        return ast.fix_missing_locations(node)
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
         self.insert_CustomFunction(node)
         ast.NodeTransformer.generic_visit(self, node)
         # print(astor.dump_tree(node))
         return ast.fix_missing_locations(node)
+
+
+def convertExpr2Expression(Expr):
+    Expr.lineno = 0
+    Expr.col_offset = 0
+    result = ast.Expression(Expr.value, lineno=0, col_offset=0)
+
+    return result
+
+
+def exec_with_return(code):
+    code_ast = ast.parse(code)
+
+    init_ast = copy.deepcopy(code_ast)
+    init_ast.body = code_ast.body[:-1]
+    # print(astor.dump_tree(init_ast.body))
+
+    last_ast = copy.deepcopy(code_ast)
+    last_ast.body = code_ast.body[-1:]
+
+    exec(compile(init_ast, "<ast>", "exec"), globals())
+    if type(last_ast.body[0]) == ast.Expr:
+        return eval(compile(convertExpr2Expression(last_ast.body[0]), "<ast>", "eval"), globals())
+    else:
+        exec(compile(last_ast, "<ast>", "exec"), globals())
+
+
+def wrap_function(tree, args, func_name="test_me"):
+    wrapper = ast.FunctionDef(name='wrapper',
+    args=ast.arguments(posonlyargs=[], args=[ast.arg(arg='trace', annotation=None, type_comment=None)], vararg=None,
+                kwonlyargs=[],
+                kw_defaults=[],
+                kwarg=None,
+                defaults=[]), decorator_list=[], returns=None, type_comment=None)
+    target_call = func_name + '('
+    for arg in args:
+        target_call += str(arg) + ', '
+    target_call += 'trace)'
+    call = ast.parse(target_call)
+    wrapper.body = [*tree.body, call.body[0]]
+    tree.body = [wrapper]
+    ast.fix_missing_locations(tree)
+    return tree
 
 
 def main():
@@ -40,15 +86,22 @@ def main():
     #     tree = ast.parse(source.read())
     import starwars.schema as schema
     tree = astor.code_to_ast(schema)
-    print(tree)
+    # print(tree)
 
-    #TODO
-    # y = profiler_transformer()
-    # y.visit(tree)
+    # TODO
+    y = profiler_transformer()
+    y.visit(tree)
     # compile(tree, "fname", 'exec')
-    # print("\n")
-    # schema = compile(tree, "fname", 'exec')
-    # print(dir(schema))
+
+    schema = compile(tree, "schema.py", "exec")
+    print(astor.dump_tree(schema))
+
+    # a = exec_with_return(schema)
+    #
+    # print(a)
+
+    ##this doesnt work because client expects a schema instance
+    # but schema is now defiined as a ast.module object
     # client = Client(schema)
     # query = """
     #     query FetchSomeIDQuery($someId: String!) {
@@ -59,8 +112,6 @@ def main():
     # """
     # params = {"someId": "3000"}
     # print(client.execute(query, variables=params))
-
-
 
     # log.info(astor.to_source(tree))
 
