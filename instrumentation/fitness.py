16136041ae2b8008937ba0a8d068ba9e23dc3c46
BoyanYK <28277932+BoyanYK@users.noreply.github.com>
2020-06-12 20:55:36 +0900

    Doc fitness

--- instrumentation/fitness.py
+++ instrumentation/fitness.py
@@ -2,6 +2,16 @@ import math
 from instrumentation.execution import run_test
 
 def get_approach_level(node_path, exec_path):
+    """[summary]
+    This function iterates through both the expected and execution path and calculates the approach level
+    Returns values necessary for fitness function calculation and condition satisfaction
+    Args:
+        node_path (list): Expected path to a node based on instrumentation
+        exec_path (list): Execution trace of nodes after running a test
+
+    Returns:
+        tuple: Tuple containing (approach_level, branch/predicate value, branch_distance)
+    """
     approach = len(node_path) - 1
     branch_test = None
     branch_distance = 1000
@@ -17,10 +27,23 @@ def get_approach_level(node_path, exec_path):
 
     return approach, branch_test, branch_distance
 
-def calculate_fitness(schema, inputs, path, context_trace):
+def calculate_fitness(schema, inputs, path, context_trace, query=""):
+    """[summary]
+    This function is a helper to simplify the execution of a test and fetching the results
+    Args:
+        schema (schema): Schema object to execute against
+        inputs (list): Inputs/arguments to pass to query
+        path (list): Expected (correct) path to target
+        context_trace (list(tuple)): Actual execution path of function
+        query (str, optional): GraphQL Query/Mutation to execute. Defaults to "".
+
+    Returns:
+        tuple: Tuple containing (fitness_value, branch/predicate value, approach_level)
+    """    
     # ? context trace should contain the executed trace, alongside booleans for the branches
     # ? execution trace would be a list of tuples as its easiest to handle without needing to insert complex structures as part of instrumentation
     # Unpack context trace - (expr, test_val, lineno, branch_dist)
+    
     query = """
         query FetchSomeIDQuery($someId: String!) {
             char(id: $someId) {
