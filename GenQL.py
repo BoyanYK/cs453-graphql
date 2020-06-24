a5ce5b4680d05efda30578f65aec8ef14200c657
devturnip <ossig.anthony@gmail.com>
2020-06-24 14:35:31 +0900

    only enable profiling when requested from cli args

--- GenQL.py
+++ GenQL.py
@@ -11,8 +11,8 @@ def main():
                         type=str, help='Path to a GraphQL schema definition file')
     parser.add_argument('-st', '--strategy', default='avm_ips',
                         type=str, help='Search strategy to use. args[avm_ips|avm_gs|rs]')
-    parser.add_argument('-p', '--profiler', default=1,
-                        type=int, help='number of times to repeat runs. default is 1.')
+    parser.add_argument('-p', '--profiler', default=0,
+                        type=int, help='number of times to repeat runs. default is 0.')
     args = parser.parse_args()
 
     profiler = args.__dict__['profiler']
@@ -21,22 +21,25 @@ def main():
     total_time = 0
     countFunctionCalls.counter = 0
     temp = 0
-    for i in range(0, profiler):
-        try:
-            print("Run:", i+1)
-            start = timeit.default_timer()
-            run(args.graphene_schema, args.graphql_schema, args.strategy)
-            stop = timeit.default_timer()
-            total_time += (stop-start)
-            count += 1
-        except:
-            print("Current run failed")
-            count += 1
+    if profiler > 0:
+        for i in range(0, profiler):
+            try:
+                print("Run:", i+1)
+                start = timeit.default_timer()
+                run(args.graphene_schema, args.graphql_schema, args.strategy)
+                stop = timeit.default_timer()
+                total_time += (stop-start)
+                count += 1
+            except:
+                print("Current run failed")
+                count += 1
 
-        print("ITR_Total loops:", countFunctionCalls.counter-temp, "| ITR_Total time taken(secs):",stop-start)
-        temp = countFunctionCalls.counter
-    print("\nTotal runs taken:", count, "| Total time taken(secs):", total_time, "| Average Time Taken(secs):", total_time/count)
-    print("Total loops:", countFunctionCalls.counter, "| Average loops:", countFunctionCalls.counter/count)
+            print("ITR_Total loops:", countFunctionCalls.counter-temp, "| ITR_Total time taken(secs):",stop-start)
+            temp = countFunctionCalls.counter
+        print("\nTotal runs taken:", count, "| Total time taken(secs):", total_time, "| Average Time Taken(secs):", total_time/count)
+        print("Total loops:", countFunctionCalls.counter, "| Average loops:", countFunctionCalls.counter/count)
+    else :
+        run(args.graphene_schema, args.graphql_schema, args.strategy)
 
 
 if __name__ == "__main__":
